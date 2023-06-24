5d00acb9882a4c1dc556889a84707a756fd937ec
Lu <102192546+CampoLucas@users.noreply.github.com>
2023-06-24 20:27:37 -0300

    Damage Box Collider

---
+++ AI Project I/Assets/_Main/Scripts/Items/Weapons/DamageBoxCollider.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Game.SO;
+using Game.Sheared;
+using Game.Entities;
+
+namespace Game.Items.Weapons
+{
+    public class DamageBoxCollider : MonoBehaviour
+    {
+        [SerializeField] private bool destroyOnEnter;
+        [SerializeField] private float damage;
+        private Damageable _damageable;
+        private WeaponSO _data;
+        private Dictionary<GameObject, Damageable> _damageables = new();
+        
+
+        public void InitData(WeaponSO data)
+        {
+            _data = data;
+        }
+
+        private void Awake()
+        {
+            _damageable = GetComponentInParent<Damageable>();
+        }
+        private void Damage(Damageable damageable)
+        {
+            damageable.TakeDamage(damage);
+        }
+
+        // private void OnTriggerEnter(Collider other)
+        // {
+        //     var otherGameObject = other.gameObject;
+        //     if (_damageables.TryGetValue(otherGameObject, out var damageable))
+        //     {
+        //         Damage(damageable);
+        //     }
+        //     else
+        //     {
+        //         damageable = otherGameObject.GetComponent<Damageable>();
+        //         if (damageable)
+        //         {
+        //             _damageables[otherGameObject] = damageable;
+        //             Damage(damageable);
+        //         }
+        //     }
+        //     if (destroyOnEnter) Destroy(this);
+        // }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            var otherGameObject = other.gameObject;
+            if (_damageables.TryGetValue(otherGameObject, out var damageable))
+            {
+                if (_damageable && damageable == _damageable) return;
+                Damage(damageable);
+            }
+            else
+            {
+                damageable = otherGameObject.GetComponent<Damageable>();
+                if (damageable)
+                {
+                    _damageables[otherGameObject] = damageable;
+                    if (_damageable && damageable == _damageable) return;
+                    Damage(damageable);
+                }
+            }
+            if (destroyOnEnter) Destroy(gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            _data = null;
+            _damageables = null;
+        }
+    }
+}
